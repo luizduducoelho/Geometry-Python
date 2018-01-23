66d34bf4f0d28d1afc0bfb6f2dc3c1ddecc4b30b
Luiz Coelho <lelcoelho@yahoo.com.br>
2018-01-23 13:43:49 -0200

    Implemented point in polygon

---
+++ point_in_polygon.py
@@ -0,0 +1,40 @@
+def polygon_inside_points(P, Q):
+    '''
+    Check which vertices of P are inside Q and return them
+    '''
+    points = []
+    for point in P:
+        i = point_in_polygon(Q)
+        if i:
+            points.append()
+    return points
+    
+
+def point_in_polygon(P, a):
+    '''
+    Check if point a is inside polygon P
+    If it is, return a 
+    '''
+    
+    # Assure that P is convex and its points are in counter-clockwise order 
+    P = gift_wrapper(P)
+    
+    n = len(P)
+    for i in range(n):
+        j = (i+1)%n
+        if not left(P[i], P[j], a):
+            return False
+    
+    return True
+    
+
+def left(a,b,c):
+    '''
+    Calculate cross product between vectors AB and AC
+    If positive, c is to the left of line ab, return True
+    '''
+    V = b - a
+    U = c - a
+    area = np.cross(V, U)
+    return area > 0
+
