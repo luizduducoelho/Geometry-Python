e1945d95efbb222209a3ef7532577135cfdd8388
Luiz Coelho <lelcoelho@yahoo.com.br>
2018-01-25 23:13:29 -0200

    Removed gift wrapper from point_in_polygon to improve performance. Now it is a requirement for the function to have cck sorted polygons

--- point_in_polygon.py
+++ point_in_polygon.py
@@ -1,10 +1,21 @@
 import numpy as np
 from poly_poly_intersect import gift_wrapper
 
+def is_convex(P):
+    
+    n = len(P)
+    m = len(gift_wrapper(P))
+    
+    return m == n 
+
 
 def polygon_inside_points(P, Q):
     '''
     Check which vertices of P are inside Q and return them
+
+    The function expect the points to be sequncialy in order
+    If thats not the case, run gift_wrapper in the points before 
+    calling this function
     '''
     points = []
     for point in P:
@@ -17,10 +28,14 @@ def point_in_polygon(P, a):
     '''
     Check if point a is inside polygon P
     Return True or False
+
+    The function expect the points to be sequncialy in order
+    If thats not the case, run gift_wrapper in the points before 
+    calling this function
     '''
     
     # Assure that P is convex and its points are in counter-clockwise order 
-    P = gift_wrapper(P)
+    #P = gift_wrapper(P)
     
     n = len(P)
     for i in range(n):
